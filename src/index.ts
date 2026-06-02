0d4c0a44c43cbf267205983fcea0f38ff398d176
Konstantinos Mavrikas <mavrikas.konstantinos@gmail.com>
2026-06-02 18:07:59 +0300

    Add plugin entry.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,39 @@
 import type { ESLint } from 'eslint';
+import { createRequire } from 'node:module';
+import noHardcodedColors from './rules/no-hardcoded-colors';
+import noDefaultPalette from './rules/no-default-palette';
+
+const { version } = createRequire(__filename)('../package.json') as { version: string };
+
+const NAMESPACE = 'tailwind-design-tokens';
+
+const recommendedRules = {
+  [`${NAMESPACE}/no-hardcoded-colors`]: 'error',
+  [`${NAMESPACE}/no-default-palette`]: 'error',
+} as const;
 
 const plugin: ESLint.Plugin = {
   meta: {
     name: 'eslint-plugin-tailwind-design-tokens',
-    version: '0.0.0',
+    version,
   },
-  rules: {},
+  rules: {
+    'no-hardcoded-colors': noHardcodedColors,
+    'no-default-palette': noDefaultPalette,
+  },
   configs: {},
 };
+
+plugin.configs!.recommended = {
+  plugins: [NAMESPACE],
+  rules: recommendedRules,
+};
+
+plugin.configs!['flat/recommended'] = [
+  {
+    plugins: { [NAMESPACE]: plugin },
+    rules: recommendedRules,
+  },
+];
 
 export = plugin;
